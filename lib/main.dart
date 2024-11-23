4835b014e676d528fcb893ebc68e4a048320e80b
dipondeb <developerdipon@gmail.com>
2024-11-23 21:10:19 +0600

    add firebase notification

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:score_app/app.dart';
+import 'package:score_app/firebase_notification_service.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -8,5 +9,7 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  await FirebaseNotificationService.initialize();
+  print(await FirebaseNotificationService.getFcmToken());
   runApp(const LiveScoreApp());
 }
